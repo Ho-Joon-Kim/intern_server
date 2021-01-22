7994afa5b1cc4ca23388cfce1e8c4e13e42df5c7
TitanV <a01098930999@gmail.com>
2021-01-22 11:47:36 +0900

    add - 폴더 구조 수정 및 mssql모듈 추가

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,9 +1,31 @@
+import sql from 'mssql';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  user: process.env.user,
+  password: process.env.password,
+  server: `${process.env.server}`,
+  database: process.env.database,
+  options: {
+    "encrypt": false,
+    "enableArithAbort": true
+  },pool: {
+    max: 20,
+    min: 10,
+    idleTimeoutMillis: 3000
+  }
+}
 
 
-export const login = (async (ctx,next) => {
-  console.log("asdasdasdad");
-  
 
+export const login = (async (ctx) => {
+  const pool = await sql.connect(config);
+  let result1 = await pool.request()
+  .query('select * from temp order by [index]');
+        
   ctx.status = 200;
-  ctx.body = 'body';
+  ctx.body = {
+  "rowsAffected" : result1.rowsAffected,
+  "recordset" : result1.recordset};
 });
